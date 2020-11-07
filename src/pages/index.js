5679525a5ed9e65b724173b8f287e2a8b6981564
lucas <luukasa2@hotmail.com>
2020-11-07 22:27:53 +0200

    removed unneeded api call

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import { getAllRestaurants } from '../data/restaurants'
 
 export default function Home() {
-  const restaurants = getAllRestaurants()
-  console.log('restaurants:', restaurants)
   return <div>Hello world!</div>
 }
