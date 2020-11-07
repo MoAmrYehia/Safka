474dbd28f3c31ac7262ddb6a94e1767e473e55d0
turq84 <kayla.m.gordon@gmail.com>
2020-11-07 16:57:15 +0200

    Updated packages and config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,24 +6,87 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [
-    'gatsby-plugin-top-layout',
-  {
-    resolve: 'gatsby-plugin-material-ui',
-    // If you want to use styled components you should change the injection order.
-    options: {
-      // stylesProvider: {
-      //   injectFirst: true,
-      // },
+  siteMetadata: {
+    title: 'Safka',
+    author: {
+      name: `Junction Team`,
+      summary: `We build things.`,
+    },
+    description: `A starter blog demonstrating what Gatsby can do.`,
+    siteUrl: `http://www.example.com`,
+    social: {
+      twitter: ``,
     },
   },
-  // If you want to use styled components you should add the plugin here.
-  'gatsby-plugin-styled-components',
-  'gatsby-plugin-react-helmet',
-],
- siteMetadata: {
-  title: 'Safka',
- // @ts-ignore
- }]
+  plugins: [
+    // `gatsby-plugin-top-layout`,
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     path: `${__dirname}/content/blog`,
+    //     name: `blog`,
+    //   },
+    // },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+
+    {
+      resolve: 'gatsby-plugin-material-ui',
+      // If you want to use styled components you should change the injection order.
+      options: {
+        // stylesProvider: {
+        //   injectFirst: true,
+        // },
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     //trackingId: `ADD YOUR TRACKING ID HERE`,
+    //   },
+    // },
+    // `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Safka`,
+        short_name: `Safka`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        // icon: `content/assets/gatsby-icon.png`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+  ],
 }
